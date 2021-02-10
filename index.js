b636f34d46910cc03f99eb4646b2d84b60259ff1
Allan5829 <allan.farca@gmail.com>
2021-02-09 20:23:21 -0800

    setup code and comments.

---
+++ index.js
@@ -0,0 +1,16 @@
+function only_5_and_3(n) {
+	console.log("test")
+}
+
+only_5_and_3(25);
+
+/*
+examples
+note only return true or false
+only5and3(14) ➞ true
+// 14 = 3*3 + 5
+only5and3(25) ➞ true
+// 25 = 5+5+5+5+5
+only5and3(7) ➞ false
+// There exists no path to the target number 7
+*/
